035358073cb9731eb5c0a651a0722587389e889e
KiranKashid <127755649+KiranKashid@users.noreply.github.com>
2023-04-12 20:24:15 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -35,7 +35,7 @@ const todoList = () => {
   const toDisplayableList = (list) => {
     return list
      .map(
-        (todo) => ${todo.completed ? '[x]' : '[ ]'} ${todo.title} ${todo.kDate === today ? '' : todo.kDate}`,
+        (todo) => `${todo.completed ? '[x]' : '[ ]'} ${todo.title} ${todo.kDate === today ? '' : todo.kDate}`,
       )
       .join('\n');
   };
