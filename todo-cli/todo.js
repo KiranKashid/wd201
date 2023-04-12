28df3751e803a5f28836788d6f08bd5bf7807427
KiranKashid <127755649+KiranKashid@users.noreply.github.com>
2023-04-12 15:48:06 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -6,6 +6,7 @@ const todoList = () => {
   const markAsComplete = (index) => {
     all[index].completed = true
   };
+  
 
   const overdue = () => {
     const k = new Date();
@@ -14,12 +15,14 @@ const todoList = () => {
     return all.filter((task) => task.kDate < k.toLocaleDateString("en-CA"));
   };
 
+
   const dueToday = () => {
    const k = new Date();
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
     return all.filter((task) => task.kDate == k.toLocaleDateString("en-CA"));
   };
+  
 
   const dueLater = () => {
     const k = new Date();
@@ -28,6 +31,7 @@ const todoList = () => {
    return all.filter((task) => task.kDate > k.toLocaleDateString("en-CA"));
   };
 
+
   const toDisplayableList = (list) => {
     return list
      .map(
@@ -35,6 +39,8 @@ const todoList = () => {
       )
       .join('\n');
   };
+  
+  
     // Format the To-Do list here, and return the output string
     // as per the format given above.
 
@@ -48,6 +54,7 @@ const todoList = () => {
     toDisplayableList
   };
 };
+
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
